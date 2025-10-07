0439c90a8e3428a145b55fc47b9cca3df0501c2d
shameer0011 <56732657+shameer0011@users.noreply.github.com>
2025-10-07 22:06:41 +0530

    Add files via upload

---
+++ ServieceRegistryApp/src/test/java/com/example/ServieceRegistryApp/ServieceRegistryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ServieceRegistryApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServieceRegistryAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
